e636bde527405b3f74ba2aaff7a10e3b26d76a18
Emile Goeminne <emile.goeminne@student.howest.be>
2019-11-12 20:17:50 +0100

    upload project start

---
+++ Project_PokemonAPI/Models/PokemonCards.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_PokemonAPI.Models
+{
+    class PokemonCards
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+        [JsonProperty(PropertyName = "nationalPokedexNumber")]
+        public int dex { get; set; }
+        [JsonProperty(PropertyName = "imageUrlid")]
+        public string imgUrl { get; set; }
+    }
+}
---
+++ Project_PokemonAPI/Repositories/PokemonRepository.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Project_PokemonAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PokemonAPI.Repositories
+{
+    class PokemonRepository
+    {
+        private static HttpClient GetHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+
+        public static async Task<PokemonCards> GetPokemonCardsAsync()
+        {
+            string url = $"https://api.pokemontcg.io/v1/cards";
+
+            //Create and use HttpClient
+            using (HttpClient client = GetHttpClient())
+            {
+                try
+                {
+                    PokemonCards result = new PokemonCards();
+                    //Ask for JSON data
+                    string content = await client.GetStringAsync(url);
+                    //Convert data to Quote object
+                    PokemonCards cards = JsonConvert.DeserializeObject<PokemonCards>(content);
+                    foreach (var card in cards)
+                    {
+                        result.Add(card);
+                    }
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
